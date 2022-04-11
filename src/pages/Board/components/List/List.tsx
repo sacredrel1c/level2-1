2dc06613c6d4a75058472ac1a6814a106fe00dd3
sacredrel1c <tpek07@gmail.com>
2022-04-11 23:01:34 +0300

    css changes

--- src/pages/Board/components/List/List.tsx
+++ src/pages/Board/components/List/List.tsx
@@ -8,13 +8,13 @@ function List(props: { title: string; cards: ICard[] }) {
     <div className="list-container">
       <h2>
         <div className="list-title">{title}</div>
-        <hr />
-        {cards.map((card) => (
-          <div className="list-content" key={card.id}>
-            <div>{card.title}</div>
-          </div>
-        ))}
       </h2>
+      <hr />
+      {cards.map((card) => (
+        <div className="list-content" key={card.id}>
+          <div>{card.title}</div>
+        </div>
+      ))}
     </div>
   );
 }
