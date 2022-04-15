e801716cafeecd6e4c424510c0dc4df13333def5
sacredrel1c <tpek07@gmail.com>
2022-04-15 23:24:29 +0300

    variable background to boards list at home page

--- src/pages/Home/components/Board/Board.tsx
+++ src/pages/Home/components/Board/Board.tsx
@@ -2,9 +2,15 @@ import React from 'react';
 import './board.scss';
 
 function Board(props: { title: string }) {
+  const getRandomColor = Math.floor(Math.random() * 16777215).toString(16);
+  const backgroundStyle = {
+    backgroundColor: '#ffffff',
+    backgroundImage: `repeating-linear-gradient(135deg,#${getRandomColor}, #${getRandomColor} 10%, transparent 10%, transparent 50%), repeating-linear-gradient(-135deg, #${getRandomColor}, #${getRandomColor} 10%, transparent 10%, transparent 50%)`,
+    backgroundSize: '4px 4px',
+  };
   const { title } = props;
   return (
-    <div className="home-board-container">
+    <div className="home-board-container" style={backgroundStyle}>
       <div className="board-title">Доска {title}</div>
     </div>
   );
